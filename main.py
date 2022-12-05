2282cd89bff5db0386f26dfddd64f52f45536531
martino1988 <61902639+martino1988@users.noreply.github.com>
2022-12-05 19:59:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, PasswordField
 from wtforms.validators import DataRequired
 import secrets
-import hashlib
 
 
 class ConfigClass(object):
     SECRET_KEY = secrets.token_hex(16)
-  
+
+
 app = Flask(__name__)
 app.config.from_object(__name__ + '.ConfigClass')
-
 
 
 class CheckPasswordForm(FlaskForm):
@@ -20,23 +19,24 @@ class CheckPasswordForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  form = CheckPasswordForm()
-  form.password.label.text = "Enter password:"
-  form.submit.label.text = "Check password"
-  if request.method == 'POST':
-    password_to_check = form.password.data
+    form = CheckPasswordForm()
+    form.password.label.text = "Enter password:"
+    form.submit.label.text = "Check password"
+    if request.method == 'POST':
+        password_to_check = form.password.data
 
-    result = get_hashes(password_to_check)
-    if result:
-      return render_template('result.html', text=result)
-    else:
-      result = "0"
-      return render_template('result.html', text=result)
-  return render_template("index.html", form=form)
+        # Check Password with api_speaker.py
+        result = get_hashes(password_to_check)
+        if result > 0:
+            return render_template('result.html', text="Password invalid!! Your password was found in {0} databreaches".format(result))
+        elif result == 0:
+            return render_template('result.html', text="Sign up successfull!")
+        else:
+            return render_template("index.html", form=form)
 
+    return render_template("index.html", form=form)
 
 
 app.run(host='0.0.0.0', port=81)
