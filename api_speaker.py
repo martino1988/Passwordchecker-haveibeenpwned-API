f78a79acb7b6b7c560f271970c4b0cb819c1ecca
martino1988 <61902639+martino1988@users.noreply.github.com>
2022-12-05 19:34:27 +0100

    Update api_speaker.py

--- api_speaker.py
+++ api_speaker.py
@@ -2,21 +2,31 @@ import requests
 
 
 # api-endpoint
-def get_hashes(digits, hash):
+def get_hashes(password_to_check):
+  
   URL = "https://api.pwnedpasswords.com/range/{0}".format(digits)
-     
-  # defining a params dict for the parameters to be sent to the API
+   
+  # get sha1-hash of password
+  encoded_str = password_to_check.encode()
+  hash_obj = hashlib.sha1(encoded_str)
+  hexa_value = hash_obj.hexdigest()
+  
+  # get first digits of hash
+  first5 = hexa_value[:5]
+  last = hexa_value[5:]
   
   # sending get request and saving the response as response object
   r = requests.get(url = URL)
   
+  # format response
   password_hashes = r.text.split()
-  hashlist = []
   
+  # split every line in hash and breach-count
+  hashlist = []
   for f in password_hashes:
     hashlist.append(f.split(":", 1))
 
-  return_hash = ""
+  return_hash = "0"
   for h in hashlist:
     if h[0] == hash.upper():
       return_hash = h[1]
